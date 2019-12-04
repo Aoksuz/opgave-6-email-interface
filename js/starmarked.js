426408b465dd951140ce307514ca9b342d5ba339
Aoksuz <Abdullahh93@gmail.com>
2019-12-04 18:02:00 +0100

    Stylet modtager feltet

---
+++ js/starmarked.js
@@ -0,0 +1,8 @@
+$(document).ready(function (e) {
+    $('.icon-yellow').on('click', function () {
+        $('this').css({
+            opacity: '1 !important',
+            fill: '$yellow'
+        })
+    });
+});
